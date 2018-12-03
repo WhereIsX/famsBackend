e9848c911da7ffda61fbbce40a78c81db9094111
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-12-03 15:31:28 -0500

    CREATE user is connected with frontend, confirmed working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :name)
+    params.require(:user).permit(:username, :password, :name)
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
 
   validates :username, { presence: true, uniqueness: true }
   # validates :password_digest, presence: true
-  validates :name, presence: true
+  # validates :name, presence: true
 
 end
