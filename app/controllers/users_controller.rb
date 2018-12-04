55d87943ca0ba6f6bf210171f94043053253cd37
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-12-03 19:31:56 -0500

    + User Serializer; + @user in UserController; + auths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
 
   def create
 
-    user = User.create(user_params)
-    if user.valid?
-      render json: {id: user.id, username: user.username}
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token}, status: :created
     else
-      render json: user.errors.full_messages, status: 422
+      render json: user.errors.full_messages, status: :not_acceptable
     end
 
   end
@@ -22,15 +24,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :password, :name)
   end
-
-
-  # if json.stringify is still sending in the above as a stringed object "{}"
-  # then the frontend can instead send the username, password, and name
-  # unwrapped.
-  # In that case, COMMENT IN the below code.
-
-  # def user_params
-  #   params.permit(:username, :password, :name)
-  # end
 
 end
